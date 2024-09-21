1dada3b467b5a1d21b98655dca0276fac855cea3
marinamjk <141650165+marinamjk@users.noreply.github.com>
2024-09-21 08:56:50 -0300

    contacto

---
+++ codigo.js
@@ -0,0 +1,20 @@
+const $form= document.querySelector('#form')
+
+$form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(event){
+    event.preventDefault()
+    const form = new FormData($form); // Usamos $form en lugar de this
+    const response = await fetch($form.action, {
+        method: $form.method, // Usamos $form en lugar de this
+        body: form,
+        headers: {
+            'Accept': 'application/json' // Corregido el 'application:/json' a 'application/json'
+        }
+    })
+
+    if(response.ok){
+        this.reset()
+        alert('Gracias por contactarme')
+    }
+}
